ca4135be0a099c6bbed656089e400719c055b06c
Pedro Strauch <pstrauch@gmail.com>
2023-03-11 18:56:53 +0100

    modelos db conexion

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,28 +1,31 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define("activity", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "activity",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      season: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    id: {
-      type: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
-    difficulty: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    duration: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    season: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
 };
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,33 +3,37 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("country", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "country",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      flag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      continent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      population: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      area: {
+        type: DataTypes.INTEGER,
+      },
     },
-    flag: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    capital: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    continent: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subregion: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    population: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    area: {
-      type: DataTypes.INTEGER,
-    },
-  });
+    { timestamps: false }
+  );
 };
